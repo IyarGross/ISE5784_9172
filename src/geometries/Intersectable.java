00021b8c6e3e00b150f02031969de9909e8b47b9
IyarGross <iyargross10@gmail.com>
2024-07-22 15:35:58 +0300

    Update Intersectable.java

--- src/geometries/Intersectable.java
+++ src/geometries/Intersectable.java
@@ -4,8 +4,7 @@ import primitives.*;
 import primitives.Vector;
 
 /*
- * interface for geometries that should provide the method findIntersection for all the forms
- * all the information about the implementation of this method you may find on https://moodle.jct.ac.il/pluginfile.php/987458/mod_resource/content/46/4%20-%20ISE.pdf
+ interface for geometries that should provide the method findIntersection for all the forms
  * */
 public abstract class Intersectable {
     public List<Point> findIntersections(Ray ray) {
